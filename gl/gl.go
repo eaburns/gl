aba1196b876ee775dbabf8916214c3aebc281273
Ethan Burns <burns.ethan@gmail.com>
2013-06-19 12:44:48 -0400

    Move deprecated OpenGL functions to deprecated.go.

--- gl/gl.go
+++ gl/gl.go
@@ -4,9 +4,8 @@ package gl
 
 /*
 #include <GL/gl.h>
-#include <GL/glu.h>
 
-#cgo pkg-config: gl glu
+#cgo pkg-config: gl
 */
 import "C"
 
@@ -14,27 +13,6 @@ import (
 	"errors"
 	"image/color"
 )
-
-func BeginQuads() {
-	C.glBegin(C.GL_POLYGON)
-}
-
-func End() {
-	C.glEnd()
-}
-
-func Vertex2(x, y float64) {
-	C.glVertex2d(C.GLdouble(x), C.GLdouble(y))
-}
-
-func Color(col color.Color) {
-	r, g, b, a := col.RGBA()
-	C.glColor4f(
-		C.GLfloat(r)/255.0,
-		C.GLfloat(g)/255.0,
-		C.GLfloat(b)/255.0,
-		C.GLfloat(a)/255.0)
-}
 
 // ClearColor sets the color used to clear the color buffer.
 func ClearColor(col color.Color) {
